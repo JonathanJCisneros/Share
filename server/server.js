8de9789b9978d5ae71957f8230fa0d0124689019
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-22 10:59:51 -0700

    secret key created

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = 8000;
-
+require('dotenv').config();
 require('./config/mongoose.config')
 
 app.use(cors());
