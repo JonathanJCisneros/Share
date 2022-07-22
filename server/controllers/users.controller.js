8a5eef50e6726a4d1739350ba697cb164917922b
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-22 12:08:34 -0700

    login/logout backend

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -9,7 +9,32 @@ module.exports = {
 
     register : (req, res) => {
         User.create(req.body)
-            .then(user => res.json({msg : "success!", user : user}))
-            .catch(err => res.json(err))
+        .then(user => {
+            const userToken = jwt.sign({id: user._id, firstName : user.firstName, lastName : user.lastName}, process.env.SECRET_KEY);
+            res
+                .cookie("usertoken", userToken, process.env.SECRET_KEY, {httpOnly: true})
+                .json({ msg: "success!", user: user });
+        })
+        .catch(err => res.json(err));
+    },
+
+    login : async(req, res) => {
+        const user = await User.findOne({ email: req.body.email });
+        if(user === null) {
+            return res.sendStatus(400);
+        }
+        const correctPassword = await bcrypt.compare(req.body.password, user.password);
+        if(!correctPassword) {
+            return res.sendStatus(400);
+        }
+        const userToken = jwt.sign({id: user._id, firstName : user.firstName, lastName : user.lastName}, process.env.SECRET_KEY);
+        res
+            .cookie("usertoken", userToken, process.env.SECRET_KEY, {httpOnly: true})
+            .json({ msg: "success!" });
+    },
+
+    logout : (req, res) => {
+        res.clearCookie('usertoken');
+        res.sendStatus(200);
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const app = express();
 const port = 8000;
 require('dotenv').config();
 require('./config/mongoose.config')
 
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
